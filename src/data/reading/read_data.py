15932b9a845086aa245ffe34cb986cf3983e6693
Luke Ginn <luke.ginn@hotmail.com>
2025-02-19 16:30:27 +1000

    Did some clean up of read_data.py

--- src/data/reading/read_data.py
+++ src/data/reading/read_data.py
@@ -5,11 +5,13 @@ from shared.data.generate_time_series_plots import generate_time_series_plots
 from shared.data.generate_histogram_plots import generate_histogram_plots
 from src.visualisation.custom_plots import custom_plots
 from src.data.reading.read_csv import read_csv
+from src.data.reading.read_excel import read_excel
 from src.data.preprocessing.initial_preprocessing import initial_preprocessing
 
-def read_data(config):
-    data = _read_data()
+def read_data(config, file_type='csv'):
+    data = read_file(file_type)
     data = initial_preprocessing(data)
+    
     if config.export_data:
         export_data(
             data=data,
@@ -41,6 +43,11 @@ def read_data(config):
         )
     return data
 
-def _read_data():
-    data = read_csv(file_path=paths.Paths.DATA_FILE_1.value)
+def read_file(file_type):
+    if file_type == 'csv':
+        data = read_csv(file_path=paths.Paths.DATA_FILE_1.value)
+    elif file_type == 'excel':
+        data = read_excel(file_path=paths.Paths.DATA_FILE_2.value)
+    else:
+        raise ValueError("Unsupported file type")
     return data
