5177a002d53cdd14ab8b76a185567cb8008661b4
Luke Ginn <luke.ginn@hotmail.com>
2025-02-22 12:30:15 +1000

    Updated the script to turn off partial plots and model saving

--- src/model/classes/model_saver.py
+++ src/model/classes/model_saver.py
@@ -9,41 +9,45 @@ class ModelSaver:
     model_config: Dict[str, Any]
 
     def run_for_iron_concentrate_perc(self, models: Dict[str, Any]):
-        model_persistence = ModelPersistence()
-        model_persistence.save(
-            models=models,
-            path=paths.Paths.IRON_CONCENTRATE_PERC_MODELS_FOLDER.value,
-            path_suffix=self.model_config["iron_concentrate_perc"]["model"][
-                "model_name"
-            ],
-        )
+        if self.model_config.iron_concentrate_perc.model.save:
+            model_persistence = ModelPersistence()
+            model_persistence.save(
+                models=models,
+                path=paths.Paths.IRON_CONCENTRATE_PERC_MODELS_FOLDER.value,
+                path_suffix=self.model_config["iron_concentrate_perc"]["model"][
+                    "model_name"
+                ],
+            )
 
     def run_for_iron_concentrate_perc_feed_blend(self, models: Dict[str, Any]):
-        model_persistence = ModelPersistence()
-        model_persistence.save(
-            path=paths.Paths.IRON_CONCENTRATE_PERC_FEED_BLEND_MODELS_FOLDER.value,
-            models=models,
-            path_suffix=self.model_config["iron_concentrate_perc"]["model"][
-                "model_name"
-            ],
-        )
+        if self.model_config.iron_concentrate_perc.model.save:
+            model_persistence = ModelPersistence()
+            model_persistence.save(
+                path=paths.Paths.IRON_CONCENTRATE_PERC_FEED_BLEND_MODELS_FOLDER.value,
+                models=models,
+                path_suffix=self.model_config["iron_concentrate_perc"]["model"][
+                    "model_name"
+                ],
+            )
 
     def run_for_silica_concentrate_perc(self, models: Dict[str, Any]):
-        model_persistence = ModelPersistence()
-        model_persistence.save(
-            path=paths.Paths.SILICA_CONCENTRATE_PERC_MODELS_FOLDER.value,
-            models=models,
-            path_suffix=self.model_config["silica_concentrate_perc"]["model"][
-                "model_name"
-            ],
-        )
+        if self.model_config.silica_concentrate_perc.model.save:
+            model_persistence = ModelPersistence()
+            model_persistence.save(
+                path=paths.Paths.SILICA_CONCENTRATE_PERC_MODELS_FOLDER.value,
+                models=models,
+                path_suffix=self.model_config["silica_concentrate_perc"]["model"][
+                    "model_name"
+                ],
+            )
 
     def run_for_silica_concentrate_perc_feed_blend(self, models: Dict[str, Any]):
-        model_persistence = ModelPersistence()
-        model_persistence.save(
-            path=paths.Paths.SILICA_CONCENTRATE_PERC_FEED_BLEND_MODELS_FOLDER.value,
-            models=models,
-            path_suffix=self.model_config["silica_concentrate_perc"]["model"][
-                "model_name"
-            ],
-        )
+        if self.model_config.silica_concentrate_perc.model.save:
+            model_persistence = ModelPersistence()
+            model_persistence.save(
+                path=paths.Paths.SILICA_CONCENTRATE_PERC_FEED_BLEND_MODELS_FOLDER.value,
+                models=models,
+                path_suffix=self.model_config["silica_concentrate_perc"]["model"][
+                    "model_name"
+                ],
+            )
--- src/model/classes/partial_plots_generator.py
+++ src/model/classes/partial_plots_generator.py
@@ -8,65 +8,69 @@ class PartialPlotsGenerator:
     model_config: dict
 
     def run_for_iron_concentrate_perc(self, best_models, data, training_features):
-        partial_plot_processor = PartialPlotProcessor(
-            model_choice=self.model_config.iron_concentrate_perc.model.model_choice,
-            models=best_models,
-            data=data[training_features],
-            features=training_features,
-            path=paths.Paths.IRON_CONCENTRATE_PERC_PARTIAL_PLOTS_PATH.value,
-            plot_confidence_interval=self.model_config.iron_concentrate_perc.partial_plots.plot_confidence_interval,
-            plot_feature_density=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density,
-            plot_feature_density_as_histogram=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
-            number_of_bins_in_histogram=self.model_config.iron_concentrate_perc.partial_plots.number_of_bins_in_histogram,
-            grid_resolution=self.model_config.iron_concentrate_perc.partial_plots.grid_resolution,
-        )
-        partial_plot_processor.run()
+        if self.model_config.iron_concentrate_perc.partial_plots.generate:
+            partial_plot_processor = PartialPlotProcessor(
+                model_choice=self.model_config.iron_concentrate_perc.model.model_choice,
+                models=best_models,
+                data=data[training_features],
+                features=training_features,
+                path=paths.Paths.IRON_CONCENTRATE_PERC_PARTIAL_PLOTS_PATH.value,
+                plot_confidence_interval=self.model_config.iron_concentrate_perc.partial_plots.plot_confidence_interval,
+                plot_feature_density=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density,
+                plot_feature_density_as_histogram=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
+                number_of_bins_in_histogram=self.model_config.iron_concentrate_perc.partial_plots.number_of_bins_in_histogram,
+                grid_resolution=self.model_config.iron_concentrate_perc.partial_plots.grid_resolution,
+            )
+            partial_plot_processor.run()
 
     def run_for_iron_concentrate_perc_feed_blend(
         self, best_models, data, training_features
     ):
-        partial_plot_processor = PartialPlotProcessor(
-            model_choice=self.model_config.iron_concentrate_perc.model.model_choice,
-            models=best_models,
-            data=data[training_features],
-            features=training_features,
-            path=paths.Paths.IRON_CONCENTRATE_PERC_FEED_BLEND_PARTIAL_PLOTS_PATH.value,
-            plot_confidence_interval=self.model_config.iron_concentrate_perc.partial_plots.plot_confidence_interval,
-            plot_feature_density=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density,
-            plot_feature_density_as_histogram=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
-            number_of_bins_in_histogram=self.model_config.iron_concentrate_perc.partial_plots.number_of_bins_in_histogram,
-            grid_resolution=self.model_config.iron_concentrate_perc.partial_plots.grid_resolution,
-        )
-        partial_plot_processor.run()
+        if self.model_config.iron_concentrate_perc.partial_plots.generate:
+            partial_plot_processor = PartialPlotProcessor(
+                model_choice=self.model_config.iron_concentrate_perc.model.model_choice,
+                models=best_models,
+                data=data[training_features],
+                features=training_features,
+                path=paths.Paths.IRON_CONCENTRATE_PERC_FEED_BLEND_PARTIAL_PLOTS_PATH.value,
+                plot_confidence_interval=self.model_config.iron_concentrate_perc.partial_plots.plot_confidence_interval,
+                plot_feature_density=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density,
+                plot_feature_density_as_histogram=self.model_config.iron_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
+                number_of_bins_in_histogram=self.model_config.iron_concentrate_perc.partial_plots.number_of_bins_in_histogram,
+                grid_resolution=self.model_config.iron_concentrate_perc.partial_plots.grid_resolution,
+            )
+            partial_plot_processor.run()
 
     def run_for_silica_concentrate_perc(self, best_models, data, training_features):
-        partial_plot_processor = PartialPlotProcessor(
-            model_choice=self.model_config.silica_concentrate_perc.model.model_choice,
-            models=best_models,
-            data=data[training_features],
-            features=training_features,
-            path=paths.Paths.SILICA_CONCENTRATE_PERC_PARTIAL_PLOTS_PATH.value,
-            plot_confidence_interval=self.model_config.silica_concentrate_perc.partial_plots.plot_confidence_interval,
-            plot_feature_density=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density,
-            plot_feature_density_as_histogram=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
-            number_of_bins_in_histogram=self.model_config.silica_concentrate_perc.partial_plots.number_of_bins_in_histogram,
-            grid_resolution=self.model_config.silica_concentrate_perc.partial_plots.grid_resolution,
-        )
-        partial_plot_processor.run()
+        if self.model_config.silica_concentrate_perc.partial_plots.generate:
+            partial_plot_processor = PartialPlotProcessor(
+                model_choice=self.model_config.silica_concentrate_perc.model.model_choice,
+                models=best_models,
+                data=data[training_features],
+                features=training_features,
+                path=paths.Paths.SILICA_CONCENTRATE_PERC_PARTIAL_PLOTS_PATH.value,
+                plot_confidence_interval=self.model_config.silica_concentrate_perc.partial_plots.plot_confidence_interval,
+                plot_feature_density=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density,
+                plot_feature_density_as_histogram=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
+                number_of_bins_in_histogram=self.model_config.silica_concentrate_perc.partial_plots.number_of_bins_in_histogram,
+                grid_resolution=self.model_config.silica_concentrate_perc.partial_plots.grid_resolution,
+            )
+            partial_plot_processor.run()
 
     def run_for_silica_concentrate_perc_feed_blend(
         self, best_models, data, training_features
     ):
-        partial_plot_processor = PartialPlotProcessor(
-            model_choice=self.model_config.silica_concentrate_perc.model.model_choice,
-            models=best_models,
-            data=data[training_features],
-            features=training_features,
-            path=paths.Paths.SILICA_CONCENTRATE_PERC_FEED_BLEND_PARTIAL_PLOTS_PATH.value,
-            plot_confidence_interval=self.model_config.silica_concentrate_perc.partial_plots.plot_confidence_interval,
-            plot_feature_density=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density,
-            plot_feature_density_as_histogram=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
-            number_of_bins_in_histogram=self.model_config.silica_concentrate_perc.partial_plots.number_of_bins_in_histogram,
-            grid_resolution=self.model_config.silica_concentrate_perc.partial_plots.grid_resolution,
-        )
-        partial_plot_processor.run()
+        if self.model_config.silica_concentrate_perc.partial_plots.generate:
+            partial_plot_processor = PartialPlotProcessor(
+                model_choice=self.model_config.silica_concentrate_perc.model.model_choice,
+                models=best_models,
+                data=data[training_features],
+                features=training_features,
+                path=paths.Paths.SILICA_CONCENTRATE_PERC_FEED_BLEND_PARTIAL_PLOTS_PATH.value,
+                plot_confidence_interval=self.model_config.silica_concentrate_perc.partial_plots.plot_confidence_interval,
+                plot_feature_density=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density,
+                plot_feature_density_as_histogram=self.model_config.silica_concentrate_perc.partial_plots.plot_feature_density_as_histogram,
+                number_of_bins_in_histogram=self.model_config.silica_concentrate_perc.partial_plots.number_of_bins_in_histogram,
+                grid_resolution=self.model_config.silica_concentrate_perc.partial_plots.grid_resolution,
+            )
+            partial_plot_processor.run()
